26232b502e8e50d9daba8bbc702634ada330a142
AbbyWahyuSadewa <125106565+AbbyWahyuSadewa@users.noreply.github.com>
2024-08-05 12:29:53 +0700

    first commit

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,27 @@
+<div class="w-full">
+    {{-- Header --}}
+    <header>
+
+    </header>
+
+    {{-- Main --}}
+    <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
+        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+            {{-- Stories --}}
+
+            <section>
+                <ul class="flex overflow-x-auto items-center gap-2">
+                    <li class="flex flex-col justify-center w-20 gap-1 p-2">
+                        <x-avatar src="https://source.unsplash.com/500x500/?face" alt="Random Face" class="h-14 w-14" />
+                        <p class="text-xs font-medium truncate">{{ fake()->name }}</p>
+                    </li>
+                </ul>
+            </section>
+        </aside>
+
+        {{-- Sugestions --}}
+        <aside class="lg:col-span-4 border hidden lg:block p-4">
+
+        </aside>
+    </main>
+</div>
