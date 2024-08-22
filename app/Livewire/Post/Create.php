bcf45649844044db89f8d21bfe6e17bcbf430257
AbbyWahyuSadewa <125106565+AbbyWahyuSadewa@users.noreply.github.com>
2024-08-22 10:11:30 +0700

    feat:properties and methods

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire\Post;
 
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class Create extends ModalComponent
 {
+
+    use WithFileUploads;
+
+    public $media=[];
+    public $description;
+    public $location;
+    public $hide_like_view;
+    public $allow_commenting;
 
     /**
      * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
@@ -13,6 +22,16 @@ class Create extends ModalComponent
     public static function modalMaxWidth(): string
     {
         return '4xl';
+    }
+
+    function submit() {
+        dd([
+            $this->media,
+            $this->description,
+            $this->location,
+            $this->hide_like_view,
+            $this->allow_commenting,
+        ]);
     }
 
     public function render()
